717fb96a2fe0dac6b2e50afbec7be90fd9a6d263
shakti_chowdhry <shaktichowdhry28@gmail.com>
2024-04-24 00:58:28 +0530

    server product MVC structure with custom error handle moedel for known and unknown routes handled

--- Server/admin/controllers/productController.js
+++ Server/admin/controllers/productController.js
@@ -1,19 +1,46 @@
-const getAllProducts = (req, res) => {
+import HttpError from "../models/httpErrorModel.js";
+
+let DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "",
+    mrp: "",
+    discount_mrp: "",
+    image: "",
+    more_images: [],
+    description: "",
+    status: "",
+    categories: [],
+    created_at: "",
+    updated_at: "",
+  },
+];
+const getAllProducts = (req, res, next) => {
   res.json({ message: "all products" });
 };
 
-const addProduct = (req, res) => {
-  res.json({ message: "product added" });
+const addProduct = (req, res, next) => {
+  // because we added body parser
+  const { title, mrp, description } = req.body;
+  //   201 for success creation
+  res.status(201).json({ message: "product added" });
 };
-const getProduct = (req, res) => {
-    const pid = req.params.pid;
-  res.json({ message: `product ${pid} description` });
+const getProduct = (req, res, next) => {
+  const productId = req.params.pid;
+  if (false) {
+    return next(new HttpError("product not found", 404));
+  }
+  res.json({ message: `product ${productId} description` });
 };
-const deleteProduct = (req, res) => {
-  res.json({ message: "product deleted" });
+const deleteProduct = (req, res, next) => {
+  const productId = req.params.pid;
+//   200 success
+  res.status(200).json({ message: "product deleted" });
 };
-const editProduct = (req, res) => {
-  res.json({ message: `product edit` });
+const editProduct = (req, res, next) => {
+  const productId = req.params.pid;
+  const { title, mrp, description } = req.body;
+  res.status(200).json({ message: `product edit` });
 };
 
 export { getAllProducts, addProduct, getProduct, deleteProduct, editProduct };
--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,41 @@
-import 'dotenv/config';
-import express from 'express';
-import categoriesRoutes from './admin/routes/categoryRoutes.js';
-import productRoutes from './admin/routes/productRoutes.js'
+import "dotenv/config";
+import express from "express";
+import bodyParser from "body-parser";
+import HttpError from "./admin/models/httpErrorModel.js";
+import categoriesRoutes from "./admin/routes/categoryRoutes.js";
+import productRoutes from "./admin/routes/productRoutes.js";
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 5500;
 
+// middleware before reaching routes
+app.use(bodyParser.json());
 
-app.use('/admin/api/categories', categoriesRoutes);
-app.use('/admin/api/products', productRoutes);
+// all routes
+app.use("/admin/api/categories", categoriesRoutes);
+app.use("/admin/api/products", productRoutes);
 
-app.get('/', (req, res) => {
-  // res.send('<h1>Welcome to NK Server</h1>');
-  res.json({message: "Welcome to NK Server"})
+// app.get("/", (req, res) => {
+//   // res.send('<h1>Welcome to NK Server</h1>');
+//   res.json({ message: "Welcome to NK Server" });
+// });
+
+// handle unknown route
+app.use((req, res, next) => {
+  throw new HttpError("Could not find this route", 404);
+});
+
+// custom error handler
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+  res.status(err.code || 5000).json({
+    message: err.message || "An unknown error occurred",
+    stack: err.stack,
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
